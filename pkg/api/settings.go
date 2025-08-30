2a74ef4e95e0ccdcda8180557f9011481d4e4e9b
adeljck <adeljck@gmail.com>
2025-08-30 15:21:12 +0800

    更新前端

--- pkg/api/settings.go
+++ pkg/api/settings.go
@@ -7,23 +7,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var settings []struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
 func ListSettings(c *gin.Context) {
 	var settings []database.Settings
 	database.Engine.Find(&settings)
 	c.JSON(http.StatusOK, gin.H{"status": 200, "data": settings})
 }
 func EditSettings(c *gin.Context) {
-	var settings struct {
-		Wecom string `json:"wecom"`
-	}
 	if err := c.BindJSON(&settings); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	setting := database.Settings{
-		Name:  "wecom",
-		Value: settings.Wecom,
+	for _, setting := range settings {
+		data := database.Settings{
+			Name:  setting.Name,
+			Value: setting.Value,
+		}
+		database.Engine.Where("name = ?", setting.Name).Update(&data)
 	}
-	database.Engine.Where("name = ?", "wecom").Update(&setting)
-	c.JSON(http.StatusOK, gin.H{"status": 200, "data": settings})
+	c.JSON(http.StatusOK, gin.H{"status": 200, "data": "", "msg": "ok"})
 }
